d1d01b5c267d2576174d95687316251e2b21ff1b
jbshop <129910884+jbshop@users.noreply.github.com>
2023-05-05 22:55:16 +0100

    Add files via upload

---
+++ suggestions.js
@@ -0,0 +1,330 @@
+// Create an array of possible tasks
+var tasks = [
+    "Pr. Thomas George",
+    "Elizabeth Thomas",
+    "Jeffrey Thomas",
+    "Jennifer Thomas",
+    "Pr. Josemon Paulose",
+    "Sujeena Jose",
+    "Joel Jose",
+    "Joanne Jose",
+    "Thomas Lonappan",
+    "Shaini Thomas",
+    "Joshua Thomas",
+    "Babu George",
+    "Suja Babu",
+    "Sneha Babu",
+    "Serin Babu",
+    "Praveen Mathews",
+    "Lijo Varughese",
+    "Michelle",
+    "Kaleb",
+    "Prinson Thomas",
+    "Ann Prinson",
+    "Simon Malayil",
+    "Mary Simon",
+    "Renoy Simon",
+    "Ann Renoy",
+    "Nathania",
+    "George Devasia",
+    "Ancy George",
+    "Shilpa George",
+    "Sruthi George",
+    "Richu George",
+    "Jenny Varghese",
+    "Jithu George",
+    "Lydia George",
+    "Bala Sudakar",
+    "Beena Bala",
+    "Aaron Bala",
+    "Adoniah Bala",
+    "Christy Raj",
+    "Nijo Christy",
+    "Zion",
+    "Aaron",
+    "Ammini",
+    "Binu Yohannan",
+    "Angel Binu",
+    "Evanna Binu",
+    "Basil Jacob",
+    "Rakhi Basil",
+    "Rayhan",
+    "Bilgah Babu",
+    "Anne Bilgah",
+    "Jose Thankachan",
+    "Jisa Jose",
+    "Johan",
+    "Amelia",
+    "Roy John",
+    "Solly Roy",
+    "Caren",
+    "Carolyn",
+    "Siju Joseph",
+    "Jincy",
+    "Joel",
+    "Jessica",
+    "Sam C P",
+    "Rintu Sam",
+    "Suresh",
+    "Deepa",
+    "Ashwin",
+    "Ashirwad",
+  "Benny M Thankachan",
+  "Mini Benny",
+  "Joel",
+  "Joanna",
+  "Jo Alexander",
+  "Golda",
+  "Jovan",
+  "Jonale",
+  "Roshan Mathew",
+  "Cissy Roshan",
+  "Ethan",
+  "Tojan Kondoor",
+  "Grace Tojan",
+  "Olivia",
+  "Oliver",
+  "Shine Sam",
+  "Sam Kurian",
+  
+  "Sijy",
+  
+  "Olivia",
+  
+  "Ethan",
+  
+  "Safin Joseph",
+  
+  "Praisy Safin",
+  
+  "Josephus",
+  
+  "Lintu Joseph",
+  
+  "Ursula Lintu",
+  
+  "Sam Thomas",
+  
+  "Anju",
+  
+  "Anayah",
+  
+  "Pr. Ajeesh C K",
+  
+  "Mini Ajeesh",
+  
+  "Shiji",
+  
+  "Akhel Varghese",
+  
+  "Anil Parayil",
+  
+  "Jancy Anil",
+  
+  "Eliz",
+  
+  "Johnson",
+  
+  "Jomol",
+  
+  "Shilpa Joseph",
+  
+  "Nebu",
+  
+  "Suby Nebu",
+  
+  "Hanna",
+  
+  "Jeremy",
+  
+  "Rebeka",
+  
+  "Akhil Joseph",
+  
+  "Sharon Akhil",
+  
+  "Mikhayla",
+  
+  "Noah",
+  
+  "Moses",
+  
+  "Shoncy",
+  
+  "Lijo Joseph",
+  "Anumol",
+  "Ameya Benedict",
+  "Sunil Chacko",
+  "Annie Sunil",
+  "Changel Sunil",
+  "Babykutty Chenkilathu",
+  "Sis. Roseli",
+  "Benny Varghese",
+  "Christine Benny",
+  "Evelyn",
+  "Gladwin Baby",
+  "Sheethal Sunil",
+  "Shyju John",
+  "Susan Shyju",
+  "Harel",
+  "Hewitt",
+  "Sheeba Shaji",
+  "Shardan Shaji",
+  "Shalet Shaji",
+  "Sajan Samuel",
+  "Sherin Sajan",
+  "Sally Joy",
+  "Asha Joy",
+  "Jayson Joy",
+  "Jancy Sabu",
+  "Chris Sabu",
+  "Chrystelle",
+  "Wilson Ittiyera",
+  "Jainy Wilson",
+  "Feba Wilson",
+  "Hephzeeba Wilson",
+  "Esther",
+  "Shinu Maria",
+  "Jacob John",
+  "Annamma Jacob",
+  "Joji Jacob",
+  "Jijo Jacob",
+  "Tejas",
+  "Rojin Varghese",
+  "Gifty Jacob",
+  "Raelyn",
+  "Sinimol Arun",
+  "Jenifer Benny",
+  "Joel",
+  "Sherin Binoy",
+  
+"Soniya",
+
+"Laly Tom",
+
+"James Harish",
+
+"Praseema",
+
+"Sarah",
+
+"Sam",
+
+"Neha",
+
+"Ashish",
+
+"Jisha",
+
+"Evana",
+
+"Bibin Thomas",
+
+"Jeffin Bibin",
+
+"Soumya Rebix",
+
+"Blessy",
+
+"Ajay Victor",
+
+"Sneha Francis",
+
+"Jisha Thomas",
+
+"Devaraj Gopinathan",
+
+"Anusyutha Krishnan",
+
+"Darshan",
+
+"Nithin Lalu",
+
+"Reshma Nithin",
+
+"Roby Joseph",
+
+"Anu Matthew",
+
+"Serah",
+
+"Seyona",
+
+"Jacob David",
+
+"Nancy Jacob",
+
+"Jonathan",
+
+"Nathaniel",
+
+"Joyamma",
+
+"John Ninan",
+
+"Bincy John",
+
+"Hannah",
+
+"Akhilav Abraham",
+
+"Soni Akhilav",
+
+"Aksa",
+
+"Manoj Rajan",
+
+"Jenu Manoj",
+
+"Ruth",
+
+"Alka",
+
+"Austin Jose",
+
+"Shruthi Austin",
+
+"Manisha Rajan",
+
+"David Pulickel",
+
+"Sherin Robin",
+
+"Jinu",
+
+"Rini",
+
+"Samuel",
+
+"Sneha Anu Thomas",
+
+"Thomas Challumattu",
+
+"Sis. Bindu",
+
+"Sam",
+
+"Sanu",
+
+"Rojin Philip",
+
+"Jibila Rojin",
+
+"Adriel",
+
+"Leo",
+
+"Ashisha",
+
+"Lemuel",
+
+"Lizbeth",
+
+"Shine",
+
+"Lynn",
+
+"Anuja",
+
+"Timothy"
+];;
